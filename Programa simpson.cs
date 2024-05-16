23b15418a3995debdfb341b925b90892405d40a9
luisSantiago15 <601alvarezluissantiago@gmail.com>
2024-05-16 15:47:19 -0500

    codigo simpson
    Creacion y solucion torneo de videojuegos de los simpson

---
+++ Programa simpson.cs
@@ -0,0 +1,25 @@
+/******************************************************************************
+autores= santiago alvarez, diana carolina
+fecha= 16.05.24
+
+Los Simpson estan organizado un torneo de videojuegos y nesecitan 
+un programa en C# para mostrar los nombres de los participantes.completa
+el espacio en blanco para imprimir los nombres en la consola:
+
+*******************************************************************************/
+using System;
+class Proram
+{
+  static void Main() 
+  {
+    string participante1 = "Bart";
+    string participante2 = "Lisa";
+    string participante3 = "Milhouse";
+    string participante4 = "Nelson";
+    
+    Console.WriteLine("el nombre del participante 1 " + participante1);
+    Console.WriteLine("el nombre del participante 2 " + participante2);
+    Console.WriteLine("el nombre del participante 3 " + participante3);
+    Console.WriteLine("el nombre del participante 4 " + participante4);
+  }
+}
